2f0e04eb224369a669d13d23e23238e676cd8320
sonex23 <sonaccx7@gmail.com>
2021-04-11 13:39:34 +0700

    Selesai page exchange, tapi fitur belum

--- src/pages/Markets.jsx
+++ src/pages/Markets.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import Header from '../components/Header';
+import InputFilterExchange from '../components/InputFilterExchange';
+import TableAllExchange from '../components/TableAllExchange';
 
 const Markets = () =>{
     return (
         <div>
             <Header/>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h1 className="text-center">Exchange List</h1>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-4">
+                        <InputFilterExchange/>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col">
+                        <TableAllExchange/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
