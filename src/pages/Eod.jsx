f62acb410dc4d6b70201443bd4c3860f49cf98a9
ardhisr <radika.kerenz1@gmail.com>
2021-04-12 09:56:22 +0700

    implement Footer to Pages

--- src/pages/Eod.jsx
+++ src/pages/Eod.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import EodData from "../components/EodData";
+import Footer from '../components/Footer';
 
 const Eod = () =>{
     return (
