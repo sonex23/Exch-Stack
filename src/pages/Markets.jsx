e0c47b74828d6535ef8b63da3fe068880b8d1518
ardhisr <radika.kerenz1@gmail.com>
2021-04-12 10:58:46 +0700

    fixing some bug

--- src/pages/Markets.jsx
+++ src/pages/Markets.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import InputFilterExchange from '../components/InputFilterExchange';
 import TableAllExchange from '../components/TableAllExchange';
 
 const Markets = () =>{
